59ddbc7e66941c9b5f1fabb86c470b2a22f996a6
Wade Varesio <wadevaresio@gmail.com>
2021-07-03 15:43:05 -0700

    wv - Add more backend tests for SavedNotesController.java

--- src/test/java/com/varesio/wade/personaltracker/controllers/SavedNoteControllerTests.java
+++ src/test/java/com/varesio/wade/personaltracker/controllers/SavedNoteControllerTests.java
@@ -11,30 +11,29 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
-
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(value = SavedNoteController.class)
 public class SavedNoteControllerTests {
 
+    private final SavedNote savedNote1 = new SavedNote(1L, "abc", "test note");
+    @MockBean
+    SavedNoteRepository savedNoteRepository;
     @Autowired
     private MockMvc mockMvc;
-
     @Autowired
     private ObjectMapper mapper;
 
-    @MockBean
-    SavedNoteRepository savedNoteRepository;
-
-    private final SavedNote savedNote1 = new SavedNote(1L, "abc", "test note");
+    private final String DELETE_MAPPING = "/api/savedNotes/delete";
+    private final String PUT_MAPPING = "/api/savedNotes/put";
+    private final String POST_MAPPING = "/api/savedNotes/new";
 
     @Test
     public void test_newSavedNote() throws Exception {
@@ -43,7 +42,7 @@ public class SavedNoteControllerTests {
         when(savedNoteRepository.save(any(SavedNote.class))).thenReturn(savedNote1);
 
         MvcResult responseEntity = mockMvc
-                .perform(post("/api/savedNotes/new").with(csrf()).contentType(MediaType.APPLICATION_JSON)
+                .perform(post(POST_MAPPING).with(csrf()).contentType(MediaType.APPLICATION_JSON)
                         .characterEncoding("utf-8").content(requestBody))
                 .andExpect(status().isOk()).andReturn();
 
@@ -60,7 +59,7 @@ public class SavedNoteControllerTests {
         String requestBody = mapper.writeValueAsString(savedNote1);
 
         MvcResult responseEntity = mockMvc
-                .perform(put("/api/savedNotes/put").with(csrf()).contentType(MediaType.APPLICATION_JSON)
+                .perform(put(PUT_MAPPING).with(csrf()).contentType(MediaType.APPLICATION_JSON)
                         .characterEncoding("utf-8").content(requestBody))
                 .andExpect(status().isOk()).andReturn();
 
@@ -69,4 +68,52 @@ public class SavedNoteControllerTests {
         assertEquals(savedNote1, responseNote);
         verify(savedNoteRepository, times(1)).findById(savedNote1.getId());
     }
+
+    @Test
+    public void test_editNoteNotFound() throws Exception {
+        when(savedNoteRepository.findById(any(Long.class))).thenReturn(Optional.empty());
+
+        String requestBody = mapper.writeValueAsString(savedNote1);
+
+        mockMvc.perform(put(PUT_MAPPING).with(csrf()).contentType(MediaType.APPLICATION_JSON).characterEncoding("utf-8").content(requestBody))
+                .andExpect(status().isNotFound()).andReturn();
+        verify(savedNoteRepository, never()).save(any(SavedNote.class));
+    }
+
+    @Test
+    public void test_deleteEmptyNote() throws Exception {
+        when(savedNoteRepository.findById(any(Long.class))).thenReturn(Optional.empty());
+
+        String requestBody = mapper.writeValueAsString(savedNote1);
+
+        mockMvc.perform(delete(DELETE_MAPPING).with(csrf()).contentType(MediaType.APPLICATION_JSON).characterEncoding("utf-8").content(requestBody))
+                .andExpect(status().isBadRequest()).andReturn();
+        verify(savedNoteRepository, never()).delete(any(SavedNote.class));
+        verify(savedNoteRepository, times(1)).findById(savedNote1.getId());
+    }
+
+    @Test
+    public void test_deleteNoteNotEquals() throws Exception {
+        when(savedNoteRepository.findById(savedNote1.getId())).thenReturn(Optional.of(new SavedNote()));
+
+        String requestBody = mapper.writeValueAsString(savedNote1);
+        mockMvc.perform(delete(DELETE_MAPPING).with(csrf()).contentType(MediaType.APPLICATION_JSON).characterEncoding("utf-8").content(requestBody))
+                .andExpect(status().isBadRequest()).andReturn();
+        verify(savedNoteRepository, never()).delete(any(SavedNote.class));
+        verify(savedNoteRepository, times(1)).findById(any(Long.class));
+    }
+
+    @Test
+    public void test_delete() throws Exception{
+        when(savedNoteRepository.findById(savedNote1.getId())).thenReturn(Optional.of(savedNote1));
+
+        String requestBody = mapper.writeValueAsString(savedNote1);
+
+        mockMvc.perform(delete(DELETE_MAPPING).with(csrf()).contentType(MediaType.APPLICATION_JSON).characterEncoding("utf-8").content(requestBody))
+                .andExpect(status().isNoContent()).andReturn();
+
+        verify(savedNoteRepository, times(1)).findById(any(Long.class));
+        verify(savedNoteRepository, times(1)).delete(any(SavedNote.class));
+    }
+
 }
